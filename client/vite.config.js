6ace23469a2d0c4f245e4ff3fffd3fbd94ad814d
unknown <nanditanilajagi@gmail.com>
2025-03-31 20:13:50 +0530

    Added start script

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/create-order': 'http://localhost:8000', // Proxy requests to the backend
+//     },
+//   },
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "GlamLumina", // ✅ Only needed for GitHub Pages
   plugins: [react()],
-  server: {
-    proxy: {
-      '/create-order': 'http://localhost:8000', // Proxy requests to the backend
-    },
-  },
-})
-
+});
 
 
