57118d4bfb1b14633e4b806f789ce497ad794160
unknown <nanditanilajagi@gmail.com>
2025-03-31 21:52:23 +0530

    first commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,29 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom"
-import ShopContextProvider from './context/ShopContext.jsx'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import {BrowserRouter} from "react-router-dom"
+// import ShopContextProvider from './context/ShopContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-<BrowserRouter>
-  <ShopContextProvider>
-  <App />
-  </ShopContextProvider>
-</BrowserRouter>
-)
+// createRoot(document.getElementById('root')).render(
+// <BrowserRouter>
+//   <ShopContextProvider>
+//   <App />
+//   </ShopContextProvider>
+// </BrowserRouter>
+// )
+
+
+
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { HashRouter } from "react-router-dom"; // Use HashRouter instead of BrowserRouter
+import ShopContextProvider from "./context/ShopContext.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <HashRouter> {/* Change from BrowserRouter to HashRouter */}
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </HashRouter>
+);
